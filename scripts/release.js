c4f798c3be2be1dd98a4292b950790ab2308e64e
Qingfeng Liu <maculosa@163.com>
2022-04-30 15:45:07 +0800

    ci: 修改execa

--- scripts/release.js
+++ scripts/release.js
@@ -1,43 +1,43 @@
-const args = require('minimist')(process.argv.slice(2))
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
-const semver = require('semver')
-const currentVersion = require('../package.json').version
-const { prompt } = require('enquirer')
-const execa = require('execa')
+const args = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const semver = require('semver');
+const currentVersion = require('../package.json').version;
+const { prompt } = require('enquirer');
+const execa = require('execa');
 
 const preId =
   args.preid ||
-  (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0])
-const isDryRun = args.dry
-const skipTests = args.skipTests
-const skipBuild = args.skipBuild
+  (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0]);
+const isDryRun = args.dry;
+const skipTests = args.skipTests;
+const skipBuild = args.skipBuild;
 const packages = fs
   .readdirSync(path.resolve(__dirname, '../packages'))
-  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'))
+  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'));
 
-const skippedPackages = []
+const skippedPackages = [];
 
 const versionIncrements = [
   'patch',
   'minor',
   'major',
   ...(preId ? ['prepatch', 'preminor', 'premajor', 'prerelease'] : [])
-]
+];
 
-const inc = i => semver.inc(currentVersion, i, preId)
-const bin = name => path.resolve(__dirname, '../node_modules/.bin/' + name)
+const inc = i => semver.inc(currentVersion, i, preId);
+const bin = name => path.resolve(__dirname, '../node_modules/.bin/' + name);
 const run = (bin, args, opts = {}) =>
-  execa(bin, args, { stdio: 'inherit', ...opts })
+  execa(bin, args, { stdio: 'inherit', ...opts });
 const dryRun = (bin, args, opts = {}) =>
-  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
-const runIfNotDry = isDryRun ? dryRun : run
-const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg)
-const step = msg => console.log(chalk.cyan(msg))
+  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts);
+const runIfNotDry = isDryRun ? dryRun : run;
+const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg);
+const step = msg => console.log(chalk.cyan(msg));
 
 async function main() {
-  let targetVersion = args._[0]
+  let targetVersion = args._[0];
 
   if (!targetVersion) {
     // no explicit version, offer suggestions
@@ -46,7 +46,7 @@ async function main() {
       name: 'release',
       message: 'Select release type',
       choices: versionIncrements.map(i => `${i} (${inc(i)})`).concat(['custom'])
-    })
+    });
 
     if (release === 'custom') {
       targetVersion = (
@@ -56,81 +56,81 @@ async function main() {
           message: 'Input custom version',
           initial: currentVersion
         })
-      ).version
+      ).version;
     } else {
-      targetVersion = release.match(/\((.*)\)/)[1]
+      targetVersion = release.match(/\((.*)\)/)[1];
     }
   }
 
   if (!semver.valid(targetVersion)) {
-    throw new Error(`invalid target version: ${targetVersion}`)
+    throw new Error(`invalid target version: ${targetVersion}`);
   }
 
   const { yes } = await prompt({
     type: 'confirm',
     name: 'yes',
     message: `Releasing v${targetVersion}. Confirm?`
-  })
+  });
 
   if (!yes) {
-    return
+    return;
   }
 
   // run tests before release
-  step('\nRunning tests...')
+  step('\nRunning tests...');
   if (!skipTests && !isDryRun) {
-    await run(bin('jest'), ['--clearCache'])
-    await run('pnpm', ['test', '--', '--bail'])
+    await run(bin('jest'), ['--clearCache']);
+    await run('pnpm', ['test', '--', '--bail']);
   } else {
-    console.log(`(skipped)`)
+    console.log(`(skipped)`);
   }
 
   // update all package versions and inter-dependencies
-  step('\nUpdating cross dependencies...')
-  updateVersions(targetVersion)
+  step('\nUpdating cross dependencies...');
+  updateVersions(targetVersion);
 
   // build all packages with types
-  step('\nBuilding all packages...')
+  step('\nBuilding all packages...');
   if (!skipBuild && !isDryRun) {
-    await run('pnpm', ['run', 'build', '--', '--release'])
+    await run('pnpm', ['run', 'build', '--', '--release']);
     // test generated dts files
-    step('\nVerifying type declarations...')
-    await run('pnpm', ['run', 'test-dts-only'])
+    step('\nVerifying type declarations...');
+    await run('pnpm', ['run', 'test-dts-only']);
   } else {
-    console.log(`(skipped)`)
+    console.log(`(skipped)`);
   }
 
   // generate changelog
-  step('\nGenerating changelog...')
-  await run(`pnpm`, ['run', 'changelog'])
+  step('\nGenerating changelog...');
+  await run(`pnpm`, ['run', 'changelog']);
 
   // update pnpm-lock.yaml
-  step('\nUpdating lockfile...')
-  await run(`pnpm`, ['install', '--prefer-offline'])
+  step('\nUpdating lockfile...');
+  await run(`pnpm`, ['install', '--prefer-offline']);
 
-  const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+  const { stdout } = await run('git', ['diff'], { stdio: 'pipe' });
   if (stdout) {
-    step('\nCommitting changes...')
-    await runIfNotDry('git', ['add', '-A'])
-    await runIfNotDry('git', ['commit', '-m', `release: v${targetVersion}`])
+    step('\nCommitting changes...');
+    await runIfNotDry('git', ['add', '-A']);
+    await runIfNotDry('git', ['commit', '-m', `release: v${targetVersion}`]);
   } else {
-    console.log('No changes to commit.')
+    console.log('No changes to commit.');
   }
 
   // publish packages
-  step('\nPublishing packages...')
+  step('\nPublishing packages...');
   for (const pkg of packages) {
-    await publishPackage(pkg, targetVersion, runIfNotDry)
+    await publishPackage(pkg, targetVersion, runIfNotDry);
   }
 
   // push to GitHub
-  step('\nPushing to GitHub...')
-  await runIfNotDry('git', ['tag', `v${targetVersion}`])
-  await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
-  await runIfNotDry('git', ['push'])
+  step('\nPushing to GitHub...');
+  await runIfNotDry('git', ['tag', `v${targetVersion}`]);
+  await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`]);
+  await runIfNotDry('git', ['push']);
 
   if (isDryRun) {
-    console.log(`\nDry run finished - run git diff to see package changes.`)
+    console.log(`\nDry run finished - run git diff to see package changes.`);
   }
 
   if (skippedPackages.length) {
@@ -140,30 +140,30 @@ async function main() {
           '\n- '
         )}`
       )
-    )
+    );
   }
-  console.log()
+  console.log();
 }
 
 function updateVersions(version) {
   // 1. update root package.json
-  updatePackage(path.resolve(__dirname, '..'), version)
+  updatePackage(path.resolve(__dirname, '..'), version);
   // 2. update all packages
-  packages.forEach(p => updatePackage(getPkgRoot(p), version))
+  packages.forEach(p => updatePackage(getPkgRoot(p), version));
 }
 
 function updatePackage(pkgRoot, version) {
-  const pkgPath = path.resolve(pkgRoot, 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
-  pkg.version = version
-  updateDeps(pkg, 'dependencies', version)
-  updateDeps(pkg, 'peerDependencies', version)
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+  const pkgPath = path.resolve(pkgRoot, 'package.json');
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+  pkg.version = version;
+  updateDeps(pkg, 'dependencies', version);
+  updateDeps(pkg, 'peerDependencies', version);
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
 }
 
 function updateDeps(pkg, depType, version) {
-  const deps = pkg[depType]
-  if (!deps) return
+  const deps = pkg[depType];
+  if (!deps) return;
   Object.keys(deps).forEach(dep => {
     if (
       dep === 'vue' ||
@@ -171,38 +171,38 @@ function updateDeps(pkg, depType, version) {
     ) {
       console.log(
         chalk.yellow(`${pkg.name} -> ${depType} -> ${dep}@${version}`)
-      )
-      deps[dep] = version
+      );
+      deps[dep] = version;
     }
-  })
+  });
 }
 
 async function publishPackage(pkgName, version, runIfNotDry) {
   if (skippedPackages.includes(pkgName)) {
-    return
+    return;
   }
-  const pkgRoot = getPkgRoot(pkgName)
-  const pkgPath = path.resolve(pkgRoot, 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  const pkgRoot = getPkgRoot(pkgName);
+  const pkgPath = path.resolve(pkgRoot, 'package.json');
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
   if (pkg.private) {
-    return
+    return;
   }
 
-  let releaseTag = null
+  let releaseTag = null;
   if (args.tag) {
-    releaseTag = args.tag
+    releaseTag = args.tag;
   } else if (version.includes('alpha')) {
-    releaseTag = 'alpha'
+    releaseTag = 'alpha';
   } else if (version.includes('beta')) {
-    releaseTag = 'beta'
+    releaseTag = 'beta';
   } else if (version.includes('rc')) {
-    releaseTag = 'rc'
+    releaseTag = 'rc';
   }
 
   // TODO use inferred release channel after official 3.0 release
   // const releaseTag = semver.prerelease(version)[0] || null
 
-  step(`Publishing ${pkgName}...`)
+  step(`Publishing ${pkgName}...`);
   try {
     await runIfNotDry(
       // note: use of yarn is intentional here as we rely on its publishing
@@ -220,17 +220,17 @@ async function publishPackage(pkgName, version, runIfNotDry) {
         cwd: pkgRoot,
         stdio: 'pipe'
       }
-    )
-    console.log(chalk.green(`Successfully published ${pkgName}@${version}`))
+    );
+    console.log(chalk.green(`Successfully published ${pkgName}@${version}`));
   } catch (e) {
     if (e.stderr.match(/previously published/)) {
-      console.log(chalk.red(`Skipping already published: ${pkgName}`))
+      console.log(chalk.red(`Skipping already published: ${pkgName}`));
     } else {
-      throw e
+      throw e;
     }
   }
 }
 
 main().catch(err => {
-  console.error(err)
-})
+  console.error(err);
+});
